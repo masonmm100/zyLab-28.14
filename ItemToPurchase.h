6df4128fc90c55115d832c701e7954698c939d81
masonmm100 <167335819+masonmm100@users.noreply.github.com>
2024-04-22 11:40:18 -0500

    Update ItemToPurchase.h
    Edited code

--- ItemToPurchase.h
+++ ItemToPurchase.h
@@ -1,32 +1,36 @@
 #ifndef ITEM_TO_PURCHASE_H
 #define ITEM_TO_PURCHASE_H
 
+
 #include <string>
 using namespace std;
 
 class ItemToPurchase {
-private:
-    string itemName;
-    string itemDescription;
-    int itemPrice;
-    int itemQuantity;
-
-public:
-    ItemToPurchase();
-    ItemToPurchase(const string& name, const string& description, int price = 0, int quantity = 0);
-
-    void SetName(const string& name);
-    void SetDescription(const string& description);
-    void SetPrice(int price);
-    void SetQuantity(int quantity);
-
-    string GetName() const;
-    string GetDescription() const;
-    int GetPrice() const;
-    int GetQuantity() const;
-    
-    void PrintItemCost() const;
-    void PrintItemDescription() const;
+   
+   public:
+      ItemToPurchase();
+      ItemToPurchase(string name, string description, 
+                  int price, int quantity = 0);
+      
+      void SetName(string name);
+      void SetDescription(string description);
+      void SetPrice(int price);
+      void SetQuantity(int quantity);
+      
+      string GetName() const;
+      string GetDescription() const;
+      int GetPrice() const;
+      int GetQuantity() const;
+      
+      void PrintItemCost();
+      void PrintItemDescription();
+   
+   private:
+      string itemName;
+      string itemDescription;
+      int itemPrice;
+      int itemQuantity;
 };
+
 
 #endif
